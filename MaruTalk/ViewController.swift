466effb69cfb5e31bb3af2c74c7e0f44a2ec69c0
Daeyun <sweet_ray@naver.com>
2024-10-29 20:21:04 +0900

    Initial Commit

---
+++ MaruTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MaruTalk
+//
+//  Created by 권대윤 on 10/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
