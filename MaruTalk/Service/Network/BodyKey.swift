fdf5f4a54044e856f6168744478023829963ef15
Daeyun <sweet_ray@naver.com>
2024-11-04 19:12:46 +0900

    Feat: SLP 서버 API 관리용 라우터 열거형 구현

---
+++ MaruTalk/Service/Network/BodyKey.swift
@@ -0,0 +1,12 @@
+//
+//  BodyKey.swift
+//  MaruTalk
+//
+//  Created by 권대윤 on 11/4/24.
+//
+
+import Foundation
+
+enum BodyKey {
+    static let email = "email"
+}
---
+++ MaruTalk/Service/Network/Router.swift
@@ -0,0 +1,100 @@
+//
+//  Router.swift
+//  MaruTalk
+//
+//  Created by 권대윤 on 11/4/24.
+//
+
+import Foundation
+
+import Alamofire
+
+enum Router {
+    case emailValidation(String)
+}
+
+extension Router: URLRequestConvertible {
+    
+    var baseURL: String {
+        return APIURL.baseURL
+    }
+    
+    var path: String {
+        switch self {
+        case .emailValidation(_):
+            return APIURL.validEmail
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .emailValidation(_):
+            return .post
+        }
+    }
+    
+    var header: [String: String] {
+        switch self {
+        case .emailValidation(_):
+            return [
+                "Content-Type": "application/json",
+                "SesacKey": APIKey.apiKey
+            ]
+            
+        default:
+            return [:]
+        }
+    }
+    
+    var body: Data? {
+        switch self {
+        case .emailValidation(let email):
+            return try? JSONEncoder().encode([
+                BodyKey.email: email
+            ])
+            
+        default:
+            return nil
+        }
+    }
+    
+    var query: [URLQueryItem]? {
+        switch self {
+        
+            
+        default:
+            return nil
+        }
+    }
+    
+    func asURLRequest() throws -> URLRequest {
+        guard var urlComponents = URLComponents(string: baseURL + path) else {
+            throw URLError(.badURL)
+        }
+        
+        guard let url = urlComponents.url else {
+            throw URLError(.badURL)
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        request.allHTTPHeaderFields = header
+        
+        //body 데이터 셋팅
+        if method == .post || method == .put || method == .patch {
+            request.httpBody = body
+        }
+        
+        //query 데이터 셋팅
+        if method == .get {
+            urlComponents.queryItems = query
+            guard let newURL = urlComponents.url else {
+                throw URLError(.badURL)
+            }
+            request.url = newURL
+        }
+        
+        return request
+    }
+}
+
