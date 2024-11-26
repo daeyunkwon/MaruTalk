3d9dd756822fc05a127e5cd72ddd6818ab81f5cb
Daeyun <sweet_ray@naver.com>
2024-11-26 19:20:01 +0900

    Feat: 채팅 화면에서 스택뷰를 사용해서 사용자 자신이 보낸 메시지의 경우 화면 오른쪽으로 표시될 수 있도록 기능 구현

--- MaruTalk/Presentation/Common/Cell/MessageFourPhotoTextTableViewCell.swift
+++ MaruTalk/Presentation/Common/Cell/MessageFourPhotoTextTableViewCell.swift
@@ -13,7 +13,7 @@ final class MessageFourPhotoTextTableViewCell: BaseTableViewCell {
     
     //MARK: - UI Components
     
-    let profileImageView: UIImageView = {
+    private let profileImageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFill
         iv.clipsToBounds = true
@@ -21,19 +21,20 @@ final class MessageFourPhotoTextTableViewCell: BaseTableViewCell {
         return iv
     }()
     
-    let usernameLabel: UILabel = {
+    private let usernameLabel: UILabel = {
         let label = UILabel()
-        label.textColor = Constant.Color.brandWhite
-        label.font = Constant.Font.caption
+        label.textColor = .darkGray
+        label.font = Constant.Font.captionSemiBold
         label.textAlignment = .left
         return label
     }()
     
-    let messageBodyLabel: UILabel = {
+    private let messageBodyLabel: UILabel = {
         let label = UILabel()
         label.textColor = Constant.Color.textPrimary
         label.font = Constant.Font.body
         label.textAlignment = .left
+        label.numberOfLines = 0
         return label
     }()
     
@@ -44,7 +45,7 @@ final class MessageFourPhotoTextTableViewCell: BaseTableViewCell {
         return view
     }()
     
-    let firstPhotoImageView: UIImageView = {
+    private let firstPhotoImageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFill
         iv.layer.cornerRadius = 12
@@ -52,15 +53,23 @@ final class MessageFourPhotoTextTableViewCell: BaseTableViewCell {
         return iv
     }()
     
-    let secondPhotoImageView: UIImageView = {
+    private let secondPhotoImageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFill
         iv.layer.cornerRadius = 12
-        iv.layer.maskedCorners = [.layerMinXMinYCorner]
+        iv.layer.maskedCorners = [.layerMaxXMinYCorner]
         return iv
     }()
     
-    let thirdPhotoImageView: UIImageView = {
+    private let thirdPhotoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.layer.cornerRadius = 12
+        iv.layer.maskedCorners = [.layerMinXMaxYCorner]
+        return iv
+    }()
+    
+    private let fourthPhotoImageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFill
         iv.layer.cornerRadius = 12
@@ -68,15 +77,7 @@ final class MessageFourPhotoTextTableViewCell: BaseTableViewCell {
         return iv
     }()
     
-    let fourthPhotoImageView: UIImageView = {
-        let iv = UIImageView()
-        iv.contentMode = .scaleAspectFill
-        iv.layer.cornerRadius = 12
-        iv.layer.maskedCorners = [.layerMaxXMaxYCorner]
-        return iv
-    }()
-    
-    let timeLabel: UILabel = {
+    private let timeLabel: UILabel = {
         let label = UILabel()
         label.textColor = Constant.Color.textSecondary
         label.font = Constant.Font.caption
@@ -84,84 +85,157 @@ final class MessageFourPhotoTextTableViewCell: BaseTableViewCell {
         return label
     }()
     
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 5
+        stackView.alignment = .leading
+        return stackView
+    }()
+    
+    private let photoVStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 2.5
+        return stackView
+    }()
+    
+    private let firstPhotoHStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 2.5
+        return stackView
+    }()
+    
+    private let secondPhotoHStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 2.5
+        return stackView
+    }()
+    
     //MARK: - Configurations
     
     override func configureHierarchy() {
         contentView.addSubviews(
             profileImageView,
-            usernameLabel,
-            messageBodyBackView,
-            messageBodyLabel,
-            firstPhotoImageView,
-            secondPhotoImageView,
-            thirdPhotoImageView,
-            fourthPhotoImageView,
-            timeLabel
+            stackView
         )
+        
+        firstPhotoHStackView.addArrangedSubview(firstPhotoImageView)
+        firstPhotoHStackView.addArrangedSubview(secondPhotoImageView)
+        secondPhotoHStackView.addArrangedSubview(thirdPhotoImageView)
+        secondPhotoHStackView.addArrangedSubview(fourthPhotoImageView)
+        
+        photoVStackView.addArrangedSubview(firstPhotoHStackView)
+        photoVStackView.addArrangedSubview(secondPhotoHStackView)
+        
+        stackView.addArrangedSubview(usernameLabel)
+        stackView.addArrangedSubview(messageBodyBackView)
+        messageBodyBackView.addSubview(messageBodyLabel)
+        stackView.addArrangedSubview(photoVStackView)
+        stackView.addArrangedSubview(timeLabel)
     }
     
     override func configureLayout() {
         profileImageView.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(6)
-            make.leading.equalToSuperview()
+            make.leading.equalToSuperview().inset(5)
             make.size.equalTo(34)
         }
         
-        usernameLabel.snp.makeConstraints { make in
+        stackView.snp.makeConstraints { make in
             make.top.equalTo(profileImageView.snp.top)
             make.leading.equalTo(profileImageView.snp.trailing).offset(8)
             make.trailing.equalToSuperview().inset(16)
+            make.bottom.equalToSuperview().offset(-6)
         }
         
         messageBodyLabel.snp.makeConstraints { make in
-            make.top.equalTo(usernameLabel.snp.bottom).offset(13)
-            make.leading.equalTo(profileImageView.snp.trailing).offset(16)
-            make.trailing.equalToSuperview().inset(16)
-        }
-        
-        messageBodyBackView.snp.makeConstraints { make in
-            make.top.equalTo(messageBodyLabel).offset(-8)
-            make.leading.equalTo(messageBodyLabel).offset(-8)
-            make.trailing.equalTo(messageBodyLabel).offset(8)
-            make.bottom.equalTo(messageBodyLabel).offset(8)
+            make.top.equalToSuperview().offset(8)
+            make.leading.equalToSuperview().offset(8)
+            make.trailing.equalToSuperview().offset(-8)
+            make.bottom.equalToSuperview().offset(-8)
         }
         
         firstPhotoImageView.snp.makeConstraints { make in
-            make.leading.equalTo(usernameLabel)
-            make.top.equalTo(messageBodyBackView).offset(8)
             make.width.equalTo(121)
             make.height.equalTo(80)
         }
         
         secondPhotoImageView.snp.makeConstraints { make in
-            make.top.equalTo(firstPhotoImageView)
-            make.leading.equalTo(firstPhotoImageView.snp.trailing).offset(2)
             make.width.equalTo(121)
             make.height.equalTo(80)
         }
         
         thirdPhotoImageView.snp.makeConstraints { make in
-            make.top.equalTo(firstPhotoImageView.snp.bottom).offset(2)
-            make.leading.equalTo(firstPhotoImageView)
             make.width.equalTo(121)
             make.height.equalTo(80)
         }
         
         fourthPhotoImageView.snp.makeConstraints { make in
-            make.top.equalTo(firstPhotoImageView.snp.bottom).offset(2)
-            make.leading.equalTo(thirdPhotoImageView.snp.trailing).offset(2)
             make.width.equalTo(121)
             make.height.equalTo(80)
-        }
-        
-        timeLabel.snp.makeConstraints { make in
-            make.leading.equalTo(usernameLabel)
-            make.top.equalTo(firstPhotoImageView.snp.bottom).offset(8)
-            make.bottom.equalToSuperview().offset(-6)
         }
     }
     
     override func configureUI() {
+        backgroundColor = .clear
+        selectionStyle = .none
+    }
+    
+    func configureCell(data: RealmChat) {
+        if let profileImagePath = data.user?.profileImage {
+            profileImageView.setImage(imagePath: profileImagePath)
+        } else {
+            profileImageView.image = UIImage(named: "noPhotoA")
+        }
         
+        usernameLabel.text = data.user?.nickname
+        
+        if !data.content.trimmingCharacters(in: .whitespaces).isEmpty {
+            messageBodyLabel.isHidden = false
+            messageBodyBackView.isHidden = false
+            messageBodyLabel.text = data.content
+        } else {
+            messageBodyLabel.isHidden = true
+            messageBodyBackView.isHidden = true
+        }
+        
+        if !data.files.isEmpty {
+            firstPhotoImageView.isHidden = false
+            secondPhotoImageView.isHidden = false
+            thirdPhotoImageView.isHidden = false
+            fourthPhotoImageView.isHidden = false
+            for path in data.files {
+                firstPhotoImageView.setImage(imagePath: path)
+                secondPhotoImageView.setImage(imagePath: path)
+                thirdPhotoImageView.setImage(imagePath: path)
+                fourthPhotoImageView.setImage(imagePath: path)
+            }
+        } else {
+            firstPhotoImageView.isHidden = true
+            secondPhotoImageView.isHidden = true
+            thirdPhotoImageView.isHidden = true
+            fourthPhotoImageView.isHidden = true
+        }
+        
+        timeLabel.text = Date.dateToString(date: data.createdAt)
+        
+        if data.user?.userID == UserDefaultsManager.shared.userID {
+            //자신이 보낸 메시지의 경우 오른쪽 배치
+            stackView.alignment = .trailing
+            usernameLabel.isHidden = true
+            profileImageView.isHidden = true
+            messageBodyBackView.backgroundColor = Constant.Color.brandBlue
+            messageBodyLabel.textColor = Constant.Color.brandWhite
+        } else {
+            //다른 사람이 보낸 메시지의 경우 왼쪽 배치
+            stackView.alignment = .leading
+            usernameLabel.isHidden = false
+            profileImageView.isHidden = false
+            messageBodyBackView.backgroundColor = Constant.Color.brandWhite
+            messageBodyLabel.textColor = Constant.Color.textPrimary
+        }
     }
 }
--- MaruTalk/Presentation/Common/Cell/MessageOnePhotoTextTableViewCell.swift
+++ MaruTalk/Presentation/Common/Cell/MessageOnePhotoTextTableViewCell.swift
@@ -13,7 +13,7 @@ final class MessageOnePhotoTextTableViewCell: BaseTableViewCell {
     
     //MARK: - UI Components
     
-    let profileImageView: UIImageView = {
+    private let profileImageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFill
         iv.clipsToBounds = true
@@ -21,19 +21,20 @@ final class MessageOnePhotoTextTableViewCell: BaseTableViewCell {
         return iv
     }()
     
-    let usernameLabel: UILabel = {
+    private let usernameLabel: UILabel = {
         let label = UILabel()
-        label.textColor = Constant.Color.brandWhite
-        label.font = Constant.Font.caption
+        label.textColor = .darkGray
+        label.font = Constant.Font.captionSemiBold
         label.textAlignment = .left
         return label
     }()
     
-    let messageBodyLabel: UILabel = {
+    private let messageBodyLabel: UILabel = {
         let label = UILabel()
         label.textColor = Constant.Color.textPrimary
         label.font = Constant.Font.body
         label.textAlignment = .left
+        label.numberOfLines = 0
         return label
     }()
     
@@ -44,14 +45,14 @@ final class MessageOnePhotoTextTableViewCell: BaseTableViewCell {
         return view
     }()
     
-    let photoImageView: UIImageView = {
+    private let photoImageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFill
         iv.layer.cornerRadius = 12
         return iv
     }()
     
-    let timeLabel: UILabel = {
+    private let timeLabel: UILabel = {
         let label = UILabel()
         label.textColor = Constant.Color.textSecondary
         label.font = Constant.Font.caption
@@ -59,60 +60,102 @@ final class MessageOnePhotoTextTableViewCell: BaseTableViewCell {
         return label
     }()
     
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 5
+        stackView.alignment = .leading
+        return stackView
+    }()
+    
     //MARK: - Configurations
     
     override func configureHierarchy() {
-        contentView.addSubviews(
+        addSubviews(
             profileImageView,
-            usernameLabel,
-            messageBodyBackView,
-            messageBodyLabel,
-            photoImageView,
-            timeLabel
+            stackView
         )
+        
+        stackView.addArrangedSubview(usernameLabel)
+        stackView.addArrangedSubview(messageBodyBackView)
+        messageBodyBackView.addSubview(messageBodyLabel)
+        stackView.addArrangedSubview(photoImageView)
+        stackView.addArrangedSubview(timeLabel)
     }
     
     override func configureLayout() {
         profileImageView.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(6)
-            make.leading.equalToSuperview()
+            make.top.equalToSuperview().inset(6)
+            make.leading.equalToSuperview().inset(5)
             make.size.equalTo(34)
         }
         
-        usernameLabel.snp.makeConstraints { make in
+        stackView.snp.makeConstraints { make in
             make.top.equalTo(profileImageView.snp.top)
             make.leading.equalTo(profileImageView.snp.trailing).offset(8)
             make.trailing.equalToSuperview().inset(16)
+            make.bottom.equalToSuperview().offset(-6)
         }
         
         messageBodyLabel.snp.makeConstraints { make in
-            make.top.equalTo(usernameLabel.snp.bottom).offset(13)
-            make.leading.equalTo(profileImageView.snp.trailing).offset(16)
-            make.trailing.equalToSuperview().inset(16)
-        }
-        
-        messageBodyBackView.snp.makeConstraints { make in
-            make.top.equalTo(messageBodyLabel).offset(-8)
-            make.leading.equalTo(messageBodyLabel).offset(-8)
-            make.trailing.equalTo(messageBodyLabel).offset(8)
-            make.bottom.equalTo(messageBodyLabel).offset(8)
+            make.top.equalToSuperview().offset(8)
+            make.leading.equalToSuperview().offset(8)
+            make.trailing.equalToSuperview().offset(-8)
+            make.bottom.equalToSuperview().offset(-8)
         }
         
         photoImageView.snp.makeConstraints { make in
-            make.leading.equalTo(usernameLabel)
-            make.top.equalTo(messageBodyBackView).offset(8)
             make.width.equalTo(244)
             make.height.equalTo(160)
-        }
-        
-        timeLabel.snp.makeConstraints { make in
-            make.leading.equalTo(usernameLabel)
-            make.top.equalTo(photoImageView.snp.bottom).offset(8)
-            make.bottom.equalToSuperview().offset(-6)
         }
     }
     
     override func configureUI() {
+        backgroundColor = .clear
+        selectionStyle = .none
+    }
+    
+    func configureCell(data: RealmChat) {
+        if let profileImagePath = data.user?.profileImage {
+            profileImageView.setImage(imagePath: profileImagePath)
+        } else {
+            profileImageView.image = UIImage(named: "noPhotoA")
+        }
         
+        usernameLabel.text = data.user?.nickname
+        
+        if !data.content.trimmingCharacters(in: .whitespaces).isEmpty {
+            messageBodyLabel.isHidden = false
+            messageBodyBackView.isHidden = false
+            messageBodyLabel.text = data.content
+        } else {
+            messageBodyLabel.isHidden = true
+            messageBodyBackView.isHidden = true
+        }
+        
+        if let photo = data.files.first {
+            photoImageView.isHidden = false
+            photoImageView.setImage(imagePath: photo)
+        } else {
+            photoImageView.isHidden = true
+        }
+        
+        timeLabel.text = Date.dateToString(date: data.createdAt)
+        
+        if data.user?.userID == UserDefaultsManager.shared.userID {
+            //자신이 보낸 메시지의 경우 오른쪽 배치
+            stackView.alignment = .trailing
+            usernameLabel.isHidden = true
+            profileImageView.isHidden = true
+            messageBodyBackView.backgroundColor = Constant.Color.brandBlue
+            messageBodyLabel.textColor = Constant.Color.brandWhite
+        } else {
+            //다른 사람이 보낸 메시지의 경우 왼쪽 배치
+            stackView.alignment = .leading
+            usernameLabel.isHidden = false
+            profileImageView.isHidden = false
+            messageBodyBackView.backgroundColor = Constant.Color.brandWhite
+            messageBodyLabel.textColor = Constant.Color.textPrimary
+        }
     }
 }
